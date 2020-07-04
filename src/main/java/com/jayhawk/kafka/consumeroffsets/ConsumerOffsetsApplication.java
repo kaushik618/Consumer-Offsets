035e03b56dc91cb9e5d68ac431ec5e9cb57c2565
kaushik618 <kaushikkulkarni618@gmail.com>
2020-07-04 14:52:17 -0500

    first commit

---
+++ src/main/java/com/jayhawk/kafka/consumeroffsets/ConsumerOffsetsApplication.java
@@ -0,0 +1,13 @@
+package com.jayhawk.kafka.consumeroffsets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerOffsetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumerOffsetsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jayhawk/kafka/consumeroffsets/ConsumerOffsetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jayhawk.kafka.consumeroffsets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerOffsetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
